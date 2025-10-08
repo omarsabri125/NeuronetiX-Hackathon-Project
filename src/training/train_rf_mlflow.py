799989e4ae822175e32685897bbf7eb5d9c7053e
omarsabri125 <omarsabri8756@gmail.com>
2025-10-08 16:59:25 +0300

    Update main,yml

--- src/training/train_rf_mlflow.py
+++ src/training/train_rf_mlflow.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 import mlflow
 from models import train_random_forest
 from data.preprocessing import preprocessing_pipline
