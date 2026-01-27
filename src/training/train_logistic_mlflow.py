ad45c453fc9efd11ed84447920e0dde2151d4562
omarsabri125 <omarsabri8756@gmail.com>
2026-01-27 19:03:54 +0200

    Refactor training script: replace preprocessing pipeline with load_processed_data and update model saving logic

--- src/training/train_logistic_mlflow.py
+++ src/training/train_logistic_mlflow.py
@@ -1,25 +1,26 @@
 import mlflow
-from models import train_logistic_regression
-from data.preprocessing import preprocessing_pipline
-from evaluation.metrics import evaluate_model
-from utils.plotting import plot_confusion_matrix, plot_ROC_AUC
+from ..models import train_logistic_regression, save_model
+from ..evaluation.metrics import evaluate_model
+from ..utils.plotting import plot_confusion_matrix, plot_ROC_AUC
+from ..data.preprocessing import load_processed_data
 import argparse
-
-FILE_PATH = "D:/Churn_Customer/NeuronetiX-Hackathon-Project/assets/Telecom_Customers _Churn_Dataset.csv"
-
 
 def train_model(C: float, penalty: str, use_smotten, plot_name):
 
     run_name = f"LogisticRegression_SMOTTEN_{use_smotten}"
 
-    X_train, X_test, y_train, y_test = preprocessing_pipline(
-        FILE_PATH, smotten=use_smotten)
+    if use_smotten:
+        X_train, X_test, y_train, y_test, _, _ = load_processed_data(smoteenn_applied=True)
+    else:
+        X_train, X_test, y_train, y_test, _, _ = load_processed_data(smoteenn_applied=False)
 
     mlflow.set_experiment(f'churn-detection')
     with mlflow.start_run(run_name=run_name) as run:
 
         lr_model = train_logistic_regression(
             X_train, y_train, C=C, penalty=penalty, random_state=42)
+
+        save_model(lr_model, f'models/{plot_name}/logistic_regression.pkl')
 
         y_pred = lr_model.predict(X_test)
 
@@ -38,11 +39,11 @@ def train_model(C: float, penalty: str, use_smotten, plot_name):
 def main(C: float, penalty: str):
     # 1. without considering the imabalancing data
     train_model(C=C, penalty=penalty,
-                use_smotten=False, plot_name="without_smotten")
+                use_smotten=False, plot_name="without_smoteenn")
 
     # 2. with considering the imabalancing data using smotten
     train_model(C=C, penalty=penalty,
-                use_smotten=True, plot_name="with_smotten")
+                use_smotten=True, plot_name="with_smoteenn")
 
 
 if __name__ == '__main__':
