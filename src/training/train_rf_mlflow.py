efe9afec296ffa43cb10b43018ec9f5cfc26710c
omarsabri125 <omarsabri8756@gmail.com>
2026-01-27 19:05:33 +0200

    Refactor training script: replace preprocessing pipeline with load_processed_data and update model saving logic

--- src/training/train_rf_mlflow.py
+++ src/training/train_rf_mlflow.py
@@ -1,29 +1,26 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
-
 import mlflow
-from src.models import train_random_forest
-from src.data.preprocessing import preprocessing_pipline
-from src.evaluation.metrics import evaluate_model
-from src.utils.plotting import plot_confusion_matrix, plot_ROC_AUC
+from ..models import train_random_forest, save_model
+from ..data.preprocessing import load_processed_data
+from ..evaluation.metrics import evaluate_model
+from ..utils.plotting import plot_confusion_matrix, plot_ROC_AUC
 import argparse
-
-FILE_PATH = "assets/Telecom_Customers _Churn_Dataset.csv"
-
-
 
 def train_model(n_estimators, max_depth, use_smotten, plot_name):
 
     run_name = f"RandomForest_SMOTTEN_{use_smotten}"
 
-    X_train, X_test, y_train, y_test = preprocessing_pipline(
-        FILE_PATH, smotten=use_smotten)
+    if use_smotten:
+        X_train, X_test, y_train, y_test, _, _ = load_processed_data(smoteenn_applied=True)
+    else:
+        X_train, X_test, y_train, y_test, _, _ = load_processed_data(smoteenn_applied=False)
 
     mlflow.set_experiment(f'churn-detection')
     with mlflow.start_run(run_name=run_name) as run:
 
         rf_model = train_random_forest(
             X_train, y_train, n_estimators=n_estimators, max_depth=max_depth, random_state=42)
+
+        save_model(rf_model, f'models/{plot_name}/random_forest.pkl')
 
         y_pred = rf_model.predict(X_test)
 
@@ -42,11 +39,11 @@ def train_model(n_estimators, max_depth, use_smotten, plot_name):
 def main(n_estimators: int, max_depth: int):
     # 1. without considering the imabalancing data
     train_model(n_estimators=n_estimators, max_depth=max_depth,
-                use_smotten=False, plot_name="without_smotten")
+                use_smotten=False, plot_name="without_smoteenn")
 
     # 2. with considering the imabalancing data using smotten
     train_model(n_estimators=n_estimators, max_depth=max_depth,
-                use_smotten=True, plot_name="with_smotten")
+                use_smotten=True, plot_name="with_smoteenn")
 
 
 if __name__ == '__main__':
