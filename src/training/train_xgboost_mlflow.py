ff47c9f180e6269145c244aa65a99af0d1aa101e
omarsabri125 <omarsabri8756@gmail.com>
2026-01-27 19:05:40 +0200

    Refactor training script: replace preprocessing pipeline with load_processed_data and add model saving functionality

--- src/training/train_xgboost_mlflow.py
+++ src/training/train_xgboost_mlflow.py
@@ -1,19 +1,18 @@
 import mlflow
-from models import train_xgboost
-from data.preprocessing import preprocessing_pipline
-from evaluation.metrics import evaluate_model
-from utils.plotting import plot_confusion_matrix, plot_ROC_AUC
+from ..models import train_xgboost, save_model
+from ..data.preprocessing import load_processed_data
+from ..evaluation.metrics import evaluate_model
+from ..utils.plotting import plot_confusion_matrix, plot_ROC_AUC
 import argparse
-
-FILE_PATH = "D:/Churn_Customer/NeuronetiX-Hackathon-Project/assets/Telecom_Customers _Churn_Dataset.csv"
-
 
 def train_model(n_estimators, max_depth, learning_rate, use_smotten, plot_name):
 
     run_name = f"XGBOOST_SMOTTEN_{use_smotten}"
 
-    X_train, X_test, y_train, y_test = preprocessing_pipline(
-        FILE_PATH, smotten=use_smotten)
+    if use_smotten:
+        X_train, X_test, y_train, y_test, _, _ = load_processed_data(smoteenn_applied=True)
+    else:
+        X_train, X_test, y_train, y_test, _, _ = load_processed_data(smoteenn_applied=False)
 
     mlflow.set_experiment(f'churn-detection')
     with mlflow.start_run(run_name=run_name) as run:
@@ -21,6 +20,8 @@ def train_model(n_estimators, max_depth, learning_rate, use_smotten, plot_name):
         xgboost_model = train_xgboost(X_train, y_train, objective='binary:logistic', n_estimators=n_estimators,
                                       learning_rate=learning_rate,
                                       max_depth=max_depth)
+        
+        save_model(xgboost_model, f'models/{plot_name}/xgboost_model.pkl')
 
         y_pred = xgboost_model.predict(X_test)
 
@@ -39,11 +40,11 @@ def train_model(n_estimators, max_depth, learning_rate, use_smotten, plot_name):
 def main(n_estimators: int, max_depth: int, learning_rate: float):
     # 1. without considering the imabalancing data
     train_model(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate,
-                use_smotten=False, plot_name="without_smotten")
+                use_smotten=False, plot_name="without_smoteenn")
 
     # 2. with considering the imabalancing data using smotten
     train_model(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate,
-                use_smotten=True, plot_name="with_smotten")
+                use_smotten=True, plot_name="with_smoteenn")
 
 
 if __name__ == '__main__':
