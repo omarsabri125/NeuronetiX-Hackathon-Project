4bf37ba341f156735533ceaca528b88d8a3caa0f
omarsabri125 <omarsabri8756@gmail.com>
2026-01-29 14:23:31 +0200

    Update main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,41 @@
-import mlflow.sklearn
+from fastapi import FastAPI
+from .routes.base import base_router
+from .routes.predict import predict_router
+import pickle
 
-model_name = "forest_best"
-model_version = "1"
+app = FastAPI(title="Churn Prediction API")
 
-# Load the model from the Model Registry
-model_uri = f"models:/{model_name}/{model_version}"
-model = mlflow.sklearn.load_model(model_uri)
+async def startup_span():
 
-print("model loaded")
+    model_path = "models/without_smoteenn/random_forest.pkl"
+    label_encoders_path = "models/without_smoteenn/label_encoders.pkl"
+    scaler_path = "models/without_smoteenn/scaler.pkl"
+
+    with open(model_path, 'rb') as f:
+        app.model = pickle.load(f)
+    
+    with open(label_encoders_path, 'rb') as f:
+        app.label_encoder = pickle.load(f)
+    
+    with open(scaler_path, 'rb') as f:
+        app.scalar = pickle.load(f)
+
+async def shutdown_span():
+
+    if hasattr(app, 'model'):
+        del app.model
+
+    if hasattr(app, 'label_encoder'):
+        del app.label_encoder
+
+    if hasattr(app, 'scalar'):
+        del app.scalar
+
+app.on_event("startup")(startup_span)
+app.on_event("shutdown")(shutdown_span)
+
+app.include_router(base_router)
+app.include_router(predict_router)
+
+
+
